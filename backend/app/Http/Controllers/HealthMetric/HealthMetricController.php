e7c5d2b4b388feafb5daed7a1f7c80f02d2c57fb
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-04-02 14:03:11 +0300

    Added model/migrations for metrics

---
+++ backend/app/Http/Controllers/HealthMetric/HealthMetricController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\HealthMetric;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\HealthMetric;
+
+class HealthMetricController extends Controller
+{
+    public function uploadCsv(Request $request)
+    {
+
+        die("test");
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
         ];
     }
+
+    public function healthMetrics()
+    {
+        return $this->hasMany(HealthMetric::class);
+    }
 }
